55a7e5dfde415fac2ba6ff0755eba04a365b2432
Vladislavius17 <114662177+Vladislavius17@users.noreply.github.com>
2024-02-29 14:54:12 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ startBtn.addEventListener("click",()=>{
   pauseBtn.innerHTML = "pause"
   document.body.appendChild(h1)
   h1.innerHTML = "score: "
-  let counter = 1000000;
+  let counter = 10;
   let bossF = false;
   
   const ctx = canvas.getContext("2d");
@@ -36,11 +36,11 @@ bunnyImg.src = "bunny.png";
 const eggImage = new Image();
 eggImage.src = "egg.png";
 
-/* const Boss1Image = new Image();
+const Boss1Image = new Image();
 Boss1Image.src = "alfa_boss_1.png";
 
 const spoiledEggImage = new Image();
-spoiledEggImage.src = "spoiledEgg.png"; */
+spoiledEggImage.src = "spoiledEgg.png";
 
 function drawBunny() {
   ctx.fillStyle = bunny.color;
@@ -50,82 +50,83 @@ function drawBunny() {
     bunny.y - 160,
     bunny.size,
     bunny.size * 2
-    );
-  }
-  
-  let eggs = [];
-  
-  
-  function getRandomInt(max, min) {
-    return Math.floor(Math.random() * max + min);
-  }
+  );
+}
 
-  function createEgg() {
-    if(bossF == true){
+let eggs = [];
+let badEggs = [];
+
+
+function getRandomInt(max, min) {
+  return Math.floor(Math.random() * max + min);
+}
+
+function createEgg() {
+    let egg = {
+      x: getRandomInt(1170,0),
+      y: 0,
+    };
+    eggs.push(egg); 
+    if(bossF){
       let egg = {
         x: getRandomInt(1170,0),
         y: 0,
-        v: getRandomInt(2,0),
       };
-      eggs.push(egg);
-    } else{
-      let egg = {
-        x: getRandomInt(1170,0),
-        y: 0,
-        v: 1,
-      };
-      eggs.push(egg);
+      badEggs.push(egg);
     }
-    
-  }
-  
-  function killEgg() {
-    eggs.forEach((egg) => {
-      if (
-        egg.x >= bunny.x - bunny.size / 2 &&
-        egg.x + 30 <= bunny.x + bunny.size / 2 &&
-        egg.y + 40 >= bunny.y - 160 &&
-        egg.y <= bunny.y
-        ) {
-          //do poprawy hitbox jajka
+}
+
+function killEgg() {
+  eggs.forEach((egg) => {
+    if (
+      egg.x >= bunny.x - bunny.size*0.8&&
+      egg.x + 30 <= bunny.x + bunny.size*0.8&&
+      egg.y + 40 >= bunny.y - 160 &&
+      egg.y <= bunny.y
+    ) {
+      //do poprawy hitbox jajka
       eggs.shift();
       points += 1;
       if(points>=counter){
         bossF = true;
         counter = 999999;
       }
-    } else if(egg.y >= canvas.height){
+    } else if(egg.y+40 >= canvas.height){
       eggs.shift();
     }
-    
+
+    // ctx.drawImage(eggImage, egg.x, egg.y, 30, 40);
+    // egg.y += 1;
+  });
+  badEggs.forEach((egg) => {
+    if (
+      egg.x >= bunny.x - bunny.size*0.8 &&
+      egg.x + 60 <= bunny.x + bunny.size*0.8 &&
+      egg.y + 80 >= bunny.y - 160 &&
+      egg.y <= bunny.y
+    ) {
+      //do poprawy hitbox jajka
+      badEggs.shift();
+    } else if(egg.y+80 >= canvas.height){
+      badEggs.shift();
+    }
+
     // ctx.drawImage(eggImage, egg.x, egg.y, 30, 40);
     // egg.y += 1;
   });
 }
 
 function drawEggs() {
-  if(bossF == true){
-    eggs.forEach((egg) => {
-      if(egg.v == 1)
-      {
-        ctx.drawImage(eggImage, egg.x, egg.y, 30, 40);
-        egg.y += 2;
-        killEgg();
-      } else{
-        ctx.drawImage(spoiledEggImage, egg.x, egg.y, 30, 40);
-        egg.y += 5;
-        killEgg();
-      }
-      
-    });
-  } else{
-    eggs.forEach((egg) => {
-      ctx.drawImage(eggImage, egg.x, egg.y, 30, 40);
-      egg.y += 2;
-      killEgg();
-    });
-  }
-  
+  eggs.forEach((egg) => {
+    ctx.drawImage(eggImage, egg.x, egg.y, 30, 40);
+    egg.y += 2;
+    killEgg();
+  });
+  badEggs.forEach((egg) => {
+    ctx.drawImage(spoiledEggImage, egg.x, egg.y, 60, 80);
+    egg.y += 1;
+    killEgg();
+  });
 }
 
 function drawBoss(){
@@ -143,15 +144,15 @@ function update() {
     drawBunny();
     drawEggs();
     
-    
+
     if (keys["a"] && bunny.x > 0 + 50) {
       bunny.x -= speed;
     } else if (keys["d"] && bunny.x < canvas.width - 50) {
       bunny.x += speed;
     }
     h1.innerHTML = `score: ${points}`;
-  }
-  requestAnimationFrame(update);
+}
+requestAnimationFrame(update);
 }
 
 function onKeyDown(e) {
@@ -179,6 +180,5 @@ const keys = {};
 
 pauseBtn.addEventListener("click", pause);
 update();
-
 })
 // boss fight
